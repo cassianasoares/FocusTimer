34c0f1736bdfee13e3d3278ca0d9977af6b1ef61
cassianasoares <cassianassilva@hotmail.com>
2021-06-22 22:50:00 -0300

    countdown commit

--- app/src/main/java/com/demo/android/cassianass/focustimer/MainActivity.kt
+++ app/src/main/java/com/demo/android/cassianass/focustimer/MainActivity.kt
@@ -2,10 +2,40 @@ package com.demo.android.cassianass.focustimer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.demo.android.cassianass.focustimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var progress_number = 0
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        updateProgressBar()
+
+        binding.incrButton.setOnClickListener {
+            if(progress_number <= 90){
+               progress_number += 10
+                updateProgressBar()
+            }
+        }
+
+        binding.decrButton.setOnClickListener {
+            if(progress_number >= 10){
+                progress_number -= 10
+                updateProgressBar()
+            }
+        }
+
+
+    }
+
+    private fun updateProgressBar() {
+        binding.progressBar.progress = progress_number
+        binding.progressTextView.text = "$progress_number%"
     }
 }
